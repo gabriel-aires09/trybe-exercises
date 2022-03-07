9a96e98bd4131316c5fcbf02aaf56f8151d45fbb
gabriel-aires09 <aires514@gmail.com>
2022-03-07 15:07:24 -0300

    Resolvendo os exercicios do bloco 4 - variaveis

---
+++ fundamentos/bloco-4-introdução-javascript/variaveis/variaveis.js
@@ -0,0 +1,16 @@
+/* aula */
+let age = 20;                 /* nao pode nome de variavel com numero e utilizar espaco */
+age = 30;
+let fullName = "Hulk";       /* utilizar camelcase - concorva de camela */
+let name = "Roldofinho Lek poderoso"; 
+let Name = "Case sensitive";
+const ageOne = "Oi";        //Essa variavel nao pode ser mais atribuida por causa do const (constante)
+
+/* Atencao - variaveis sao case sensitive */ 
+// let voce consegue reatribuir valores nessas variavel
+
+console.log(age);
+console.log(fullName);
+console.log(name);
+console.log(Name);
+console.log(ageOne);
