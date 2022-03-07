442ddb72c44cba9565fc038045b52dcce5133f1c
gabriel-aires09 <aires514@gmail.com>
2022-03-07 17:08:04 -0300

    Resolvendo os exercicios do bloco 4 - tipos primitivos. Todos resolvidos

--- fundamentos/bloco-4-introdução-javascript/tipos-primitivos-dinamico-arit/exercicio.js
+++
@@ -1,9 +0,0 @@
-let patientId = 50;
-let isEnrolled = true;
-const patientInfo = {
-    firstName: 'Ana';
-    lastName: 'Santos';
-};
-const patientEmail = 'ana@email.com';
-
-// imprimir qual e o tipo de variaveis
