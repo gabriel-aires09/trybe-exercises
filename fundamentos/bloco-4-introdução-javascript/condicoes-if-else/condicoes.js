5fe544b107068f1cc86b31b16b095f9dee7b6d98
gabriel-aires09 <aires514@gmail.com>
2022-03-07 18:09:21 -0300

    Resolvendo os exercicios do bloco 4 - condicoes

---
+++ fundamentos/bloco-4-introdução-javascript/condicoes-if-else/condicoes.js
@@ -0,0 +1,17 @@
+//if - se
+//else - senao
+
+/*
+if (codicao){
+    //codigo - caso nossa condicao seja comprida
+}
+
+else if (outra condicao) {
+    //outra condicao - pode ser utilizados quantas vezes for necessarias.
+}
+
+else {
+    // nenhuma das condicoes anteriores forem verdadeiras, vai executar esta condicao
+}
+*/
+
